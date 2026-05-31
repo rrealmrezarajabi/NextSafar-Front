5da463d08faa764361c12d7e92533d7f7b91589c
rrealmrezarajabi <rrealmrezarajabi@gmail.com>
2026-05-31 14:58:19 +0330

    auth types added

---
+++ types/auth.types.ts
@@ -0,0 +1,52 @@
+export type Role = "user" | "owner";
+
+export interface Account {
+  id: number;
+  first_name: string;
+  last_name: string;
+  role: Role;
+}
+
+export interface UserProfile extends Account {
+  phone: string;
+  email: string;
+}
+
+export interface LoginPayload {
+  phone: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  access: string;
+  refresh: string;
+  account: Account;
+}
+
+export interface RegisterUserPayload {
+  first_name: string;
+  last_name: string;
+  phone: string;
+  email: string;
+  password: string;
+}
+
+export interface RegisterOwnerPayload extends RegisterUserPayload {
+  company_name: string;
+}
+
+export interface RegisterUserResponse {
+  refresh: string;
+  access: string;
+  user: UserProfile;
+}
+
+export interface RegisterOwnerResponse {
+  refresh: string;
+  access: string;
+  owner: UserProfile;
+}
+
+export interface RefreshTokenResponse {
+  access: string;
+}
